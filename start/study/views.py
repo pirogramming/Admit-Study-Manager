8984f0c021f8079d82d0e2a6898096cb75f62537
jinhyeon <kjh_5771@naver.com>
2019-08-06 16:48:03 +0900

    푸시푸시

--- start/study/views.py
+++ start/study/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import GroupForm, RegisterForm
 from .models import Group, Membership
-from accounts.models import StudyUser
+from django import forms
 
 # Create your views here.
 
@@ -35,11 +35,10 @@ def group_register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            user = StudyUser()
-            user.save()
-            group = Group.objects.create(group_name=request.POST['group_name'])
-            m = Membership.objects.create(person=user, group=group) #FIXME group변수를 이미 만들어진 이름으로 저장할 경우 오류!
-        return redirect(group)                                      #FIXME 현재 접속된 user의 정보로 person이 저장되도록 고쳐야 함
+            user = request.user
+            group = Group.objects.get(group_name=request.POST['group_name'])
+            m = Membership.objects.create(person=user, group=group)
+        return redirect(group)
     else:
         form = RegisterForm()
     return render(request, 'study/group_register.html', {
