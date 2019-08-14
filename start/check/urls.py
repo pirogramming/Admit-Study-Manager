8366c9e6e3a0ac14c2a526e32be26d96cb0cef38
김소현 <prrsh22@gmail.com>
2019-08-15 01:19:04 +0900

    check 에서 UpdateHistory 클래스 추가

--- start/check/urls.py
+++ start/check/urls.py
@@ -1,5 +1,9 @@
+from django.urls import path
+from check import views
+
 app_name = 'check'
 
 urlpatterns = [
-
+    path('<int:group_id>/', views.check_home, name="check_home"),
+    path('<int:group_id>/update', views.update, name="update"),
 ]
--- start/check/views.py
+++ start/check/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def check_home(request, group_id):
+    pass
+
+
+def update(request, group_id):
+    pass
