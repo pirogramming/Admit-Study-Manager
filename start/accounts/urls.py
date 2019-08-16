665cf91d3441b708f22378f525bf281130b8f12c
jinhyeon <kjh_5771@naver.com>
2019-08-16 22:12:22 +0900

    비밀번호 변경 기능 추가

--- start/accounts/urls.py
+++ start/accounts/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('profile/revise', views.profile_revise, name='profile_revise'),
+    path('password_change', views.password_change, name='password_change'),
 ]
