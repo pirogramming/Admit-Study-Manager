c6193208377c7ad74f00612feb3f5db31e24d449
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-08-23 00:58:54 +0900

    group base앞까지의 프론트엔드 완성

--- start/study/views.py
+++ start/study/views.py
@@ -25,19 +25,21 @@ from django.contrib.auth.decorators import login_required
 
 from study.models import Membership
 
-def group_required(func):
-   @functools.wraps(func)
-   def wrapper(request, id):
-       group = get_object_or_404(Group, id=id)
-       group_name = group.group_name
-       user = request.user
-       usergroup_list = [x.group.group_name for x in Membership.objects.filter(person=user, status='ACTIVE')]
-       if request.user.is_authenticated and group_name not in usergroup_list:
-           return render(request, 'study/group_reject.html', {'group_name':group_name})
-           # return HttpResponse("{}그룹 멤버가 아니므로 글을 쓸 수 없습니다.".format(group_name))
 
-       return func(request, id)
-   return wrapper
+def group_required(func):
+    @functools.wraps(func)
+    def wrapper(request, id):
+        group = get_object_or_404(Group, id=id)
+        group_name = group.group_name
+        user = request.user
+        usergroup_list = [x.group.group_name for x in Membership.objects.filter(person=user, status='ACTIVE')]
+        if request.user.is_authenticated and group_name not in usergroup_list:
+            return render(request, 'study/group_reject.html', {'group_name': group_name})
+            # return HttpResponse("{}그룹 멤버가 아니므로 글을 쓸 수 없습니다.".format(group_name))
+
+        return func(request, id)
+
+    return wrapper
 
 
 def mn_stf_required(func):
@@ -51,7 +53,9 @@ def mn_stf_required(func):
             # return render(request, 'study/group_reject.html', {'group_name': group_name})
             return HttpResponse("매니저 권한이 필요합니다.")
         return func(request, id)
+
     return wrapper
+
 
 def manager_required(func):
     @functools.wraps(func)
@@ -64,7 +68,9 @@ def manager_required(func):
             # return render(request, 'study/group_reject.html', {'group_name': group_name})
             return HttpResponse("매니저 권한이 필요합니다.")
         return func(request, id)
+
     return wrapper
+
 
 @login_required
 def all_group_list(request):
@@ -78,12 +84,12 @@ def all_group_list(request):
         'all_group_list': gs,
     })
 
+
 @login_required
 def group_list(request):
     user = request.user
     # usergroup_list = [x.group.group_name for x in Membership.objects.filter(person=user, status='ACTIVE')]
     # group_list = Group.objects.filter(group_member=user)
-
     groups = [x.group.group_name for x in Membership.objects.filter(person=user, status='ACTIVE')]
     gs = Group.objects.filter(group_name__in=groups)
     g = request.GET.get('g', '')
@@ -94,8 +100,9 @@ def group_list(request):
         'group_list': gs,
     })
 
+
 @login_required
-def all_group_detail(request,id):
+def all_group_detail(request, id):
     group = get_object_or_404(Group, id=id)
     # membership_manager = [x.person for x in Membership.objects.filter(group=group, role='MANAGER', status='ACTIVE')]
     # membership_member = [x.person for x in Membership.objects.filter(group=group, role='MEMBER', status='ACTIVE')]
@@ -145,8 +152,8 @@ def group_detail(request, id):
         'membership_member': membership_member,
         'usermembership': usermembership,
         'group': group,
-        'memberships_zip': zip(memberships, range(1, (len(memberships)+1))),
-        'penalty_list_zip': zip(penalty_list, range(1, (len(penalty_list)+1))),
+        'memberships_zip': zip(memberships, range(1, (len(memberships) + 1))),
+        'penalty_list_zip': zip(penalty_list, range(1, (len(penalty_list) + 1))),
         'latest_update': latest_update,
         'attend_posts': attend_posts,
         'notice_posts': notice_posts,
@@ -167,18 +174,18 @@ def group_detailboard(request, id):
 
     # latest_assignment = "가장 최근 과제 instance"
     latest_assignment = Assignment.objects.filter(group=group).order_by('created_at').last()
-    dones = Done.objects.filter(assignment = latest_assignment)
+    dones = Done.objects.filter(assignment=latest_assignment)
     authors = [x.author for x in dones]
     doneform = DoneForm()
     latest_attend = group.attend_set.all().order_by('pk').last()
-        # "가장 최근 출석"
+    # "가장 최근 출석"
     confirm_value = latest_attend.attendconfirm_set.filter(
         attend_user=request.user.nickname,
         attend_check='출석 정보 없음'
     )
-    #출석 여부
+    # 출석 여부
     attendform = AttendConfirmForm()
-        #"출석코드 입력해서 출석완료하는 form"
+    # "출석코드 입력해서 출석완료하는 form"
     # attend_history = "표로 이 출석에 대한 출석 결과 나옴"
 
     if request.method == 'POST':
@@ -216,7 +223,7 @@ def group_detailboard(request, id):
                         attending_member.sub_time = sub_time
                         attending_member.attend_check = '출석'
                         attending_member.save()
-                        usermembership.admit_attend += 1    # ㅇㅈ하나 추가
+                        usermembership.admit_attend += 1  # ㅇㅈ하나 추가
                         usermembership.total_admit += 1
                         usermembership.save()
                         messages.success(request, '성공적으로 출석했습니다!')
@@ -228,7 +235,7 @@ def group_detailboard(request, id):
                         attending_member.sub_time = sub_time
                         attending_member.attend_check = '지각'
                         attending_member.save()
-                        usermembership.late_attend += 1    # 지각 횟수 한번 추가
+                        usermembership.late_attend += 1  # 지각 횟수 한번 추가
                         usermembership.save()
                         messages.success(request, '지각입니다ㅜㅜ')
                         return redirect('study:group_detailboard', id)
@@ -244,27 +251,21 @@ def group_detailboard(request, id):
 
         # elif request.get('assignment', ''):
 
-    else :
+    else:
         ctx = {
-            'group':group,
-            'usermembership':usermembership,
-            'now':now,
-            'latest_assignment':latest_assignment,
-            'dones':dones,
-            'doneform':doneform,
-            'latest_attend':latest_attend,
-            'confirm_value':confirm_value,
-            'attendform':attendform,
-            'authors':authors,
+            'group': group,
+            'usermembership': usermembership,
+            'now': now,
+            'latest_assignment': latest_assignment,
+            'dones': dones,
+            'doneform': doneform,
+            'latest_attend': latest_attend,
+            'confirm_value': confirm_value,
+            'attendform': attendform,
+            'authors': authors,
         }
         return render(request, 'study/group_detailboard.html', ctx)
     # return HttpResponse("group detailboard작성중")
-
-
-
-
-
-
 
 
 @group_required
@@ -301,15 +302,14 @@ def group_update(request, group_id):
         attend.save(update_fields=['attend_data_checked'])
 
     # 벌금 산출
-    memberships.update(penalty_attend= F('late_attend') * int(group.late_penalty) + \
-                         F('noshow_attend') * int(group.abscence_penalty),
-                          penalty_assign=F('noshow_assign')*int(group.notsubmit_penalty))
-    memberships.update(total_penalty=F('penalty_attend')+F('penalty_assign'))
+    memberships.update(penalty_attend=F('late_attend') * int(group.late_penalty) + \
+                                      F('noshow_attend') * int(group.abscence_penalty),
+                       penalty_assign=F('noshow_assign') * int(group.notsubmit_penalty))
+    memberships.update(total_penalty=F('penalty_attend') + F('penalty_assign'))
 
     # 업데이트 기록 저장
     UpdateHistory.objects.create(group=group, created_at=datetime.now())
     return redirect(resolve_url('study:group_detail', group.id))
-
 
 
 @login_required
@@ -348,6 +348,11 @@ def group_new(request):
 
 def group_register(request, id):
     group = Group.objects.get(id=id)
+    membership_manager = Membership.objects.filter(group=group, role='MANAGER', status='ACTIVE')
+    membership_staff = Membership.objects.filter(group=group, role='STAFF', status='ACTIVE')
+    membership_member = Membership.objects.filter(group=group, role='MEMBER', status='ACTIVE')
+
+
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -365,6 +370,10 @@ def group_register(request, id):
                                 form = RegisterForm()
                                 return render(request, 'study/group_register.html', {
                                     'form': form,
+                                    'group': group,
+                                    'membership_manager': membership_manager,
+                                    'membership_staff': membership_staff,
+                                    'membership_member': membership_member,
                                 })
 
                         m = Membership.objects.create(person=user, group=group)
@@ -375,25 +384,44 @@ def group_register(request, id):
                         form = RegisterForm()
                         return render(request, 'study/group_register.html', {
                             'form': form,
+                            'group': group,
+                            'membership_manager': membership_manager,
+                            'membership_staff': membership_staff,
+                            'membership_member': membership_member,
                         })
                 else:
                     messages.error(request, '코드가 일치하지 않습니다.')
                     form = RegisterForm()
                     return render(request, 'study/group_register.html', {
                         'form': form,
+                        'group': group,
+                        'membership_manager': membership_manager,
+                        'membership_staff': membership_staff,
+                        'membership_member': membership_member,
                     })
             else:
                 messages.error(request, '존재하지 않는 그룹입니다.')
                 form = RegisterForm()
                 return render(request, 'study/group_register.html', {
                     'form': form,
+                    'group': group,
+                    'membership_manager': membership_manager,
+                    'membership_staff': membership_staff,
+                    'membership_member': membership_member,
                 })
     else:
         form = RegisterForm()
-    return render(request, 'study/group_register.html', {
+
+    context = {
         'form': form,
         'group': group,
-    })
+        'membership_manager': membership_manager,
+        'membership_staff': membership_staff,
+        'membership_member': membership_member,
+    }
+
+    return render(request, 'study/group_register.html', context)
+
 
 @login_required
 def group_registerbyurl(request, invitation_url):
@@ -409,9 +437,9 @@ def group_registerbyurl(request, invitation_url):
         # try:
         if user not in memberlist:
             m = Membership.objects.create(person=user, group=group)
-                # obj = Membership.objects.create(person=user, group=group)
-                # obj.status = 'ACTIVE'
-                # obj.save()
+            # obj = Membership.objects.create(person=user, group=group)
+            # obj.status = 'ACTIVE'
+            # obj.save()
             messages.success(request, '"{}"그룹에 가입했습니다!'.format(group.group_name))
             return redirect(group)
 
@@ -424,34 +452,44 @@ def group_registerbyurl(request, invitation_url):
         #     pw = request.POST['password']
         #     u = authenticate(username=id, password=pw)
 
-            # if u:
-            #     login(request, user=u)
-            #     return render(request, 'study/group_registerbyurl.html', {'group': group, 'form': form, 'membership':membership})
-            # else:
-            #     return render(request, 'study/group_registerbyurl.html', {'group': group, 'form': form, 'error':'아이디나 비밀번호가 일치하지 않습니다.', 'membership':membership})
-                # return render(request, 'study/group_registerbyurl.html', {'group':group})
+        # if u:
+        #     login(request, user=u)
+        #     return render(request, 'study/group_registerbyurl.html', {'group': group, 'form': form, 'membership':membership})
+        # else:
+        #     return render(request, 'study/group_registerbyurl.html', {'group': group, 'form': form, 'error':'아이디나 비밀번호가 일치하지 않습니다.', 'membership':membership})
+        # return render(request, 'study/group_registerbyurl.html', {'group':group})
 
-    else :
+    else:
         group = Group.objects.get(invitation_url=invitation_url)
         form = LoginForm()
-        return render(request, 'study/group_registerbyurl.html', {'group':group,
-                                                                  'form':form,
-                                                                  'membership':membership,
-                                                                  'membership_manager':membership_manager,
-                                                                  'membership_staff':membership_staff,
-                                                                  'membership_member':membership_member,})
+        return render(request, 'study/group_registerbyurl.html', {'group': group,
+                                                                  'form': form,
+                                                                  'membership': membership,
+                                                                  'membership_manager': membership_manager,
+                                                                  'membership_staff': membership_staff,
+                                                                  'membership_member': membership_member, })
+
 
 @login_required
 def group_mystudy(request):
-    return render(request, 'study/group_mystudy.html')
+    user = request.user
+    groups = [x.group.group_name for x in Membership.objects.filter(person=user, status='ACTIVE')]
+    gs = Group.objects.filter(group_name__in=groups)
+    g = request.GET.get('g', '')
+    if g:
+        gs = gs.filter(group_name__icontains=g)
+
+    return render(request, 'study/group_mystudy.html', {
+        'group_list': gs,
+    })
 
 
 @login_required
-def mystudy_list(request,id):
-    user = get_object_or_404(StudyUser,id=id)
+def mystudy_list(request, id):
+    user = get_object_or_404(StudyUser, id=id)
     groups = Membership.objects.filter(person=user, status='ACTIVE')
 
-    return render(request, 'study/mystudy_list.html',{
+    return render(request, 'study/mystudy_list.html', {
         'user': user, 'groups': groups,
     })
 
@@ -476,6 +514,7 @@ def group_mysettings(request, id):
         'group': group,
         'membership': usermembership,
     })
+
 
 @group_required
 @manager_required
@@ -571,7 +610,7 @@ def group_settings_mn(request, id):
 
         elif request.POST.get('dltGroup', ''):
             Membership.objects.filter(group=group).delete()
-            #관련 notice, attendance, assignment 모두 지우기
+            # 관련 notice, attendance, assignment 모두 지우기
             group.delete()
             return redirect('home')
 
@@ -581,6 +620,7 @@ def group_settings_mn(request, id):
         return redirect(group)
 
     return render(request, 'study/group_settings_mn.html', ctx)
+
 
 @group_required
 @mn_stf_required
@@ -661,10 +701,11 @@ def group_base(request, id):
     group = get_object_or_404(Group.objects.prefetch_related(), id=id)
     user = request.user
     ctx = {
-        'group':group,
-        'user':user,
+        'group': group,
+        'user': user,
     }
     return render(request, 'group_base.html', ctx)
+
 
 def member_info(request, id):
     membership = get_object_or_404(Membership, id=id)
@@ -673,10 +714,10 @@ def member_info(request, id):
     usermembership = get_object_or_404(Membership, group=group, person=request.user)
 
     return render(request, 'study/member_info.html', {
-        'group':group,
+        'group': group,
         'user': user,
         'membership': membership,
-        'usermembership':usermembership,
+        'usermembership': usermembership,
     })
 
 
@@ -686,11 +727,10 @@ def member_info_list(request, id):
     usermembership = get_object_or_404(Membership, group=group, person=request.user)
 
     return render(request, 'study/member_info_list.html', {
-        'group':group,
+        'group': group,
         'memberships': memberships,
-        'usermembership':usermembership,
+        'usermembership': usermembership,
     })
-
 
 
 def injung_plus(request, done_id):
@@ -712,7 +752,7 @@ def injung_plus(request, done_id):
         original_author.total_admit += 1
         done.save()
         original_author.save()
-        new_injung = Injung_history.objects.create(author=request.user, done=done,)
+        new_injung = Injung_history.objects.create(author=request.user, done=done, )
 
         memberships = Membership.objects.filter(group=done.assignment.group)
         for m in memberships:
@@ -723,5 +763,3 @@ def injung_plus(request, done_id):
             m.save()
 
         return redirect(done)
-
-
