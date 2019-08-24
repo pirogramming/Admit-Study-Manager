606c01f0840c59eaf28eaa1b219280216ea35e02
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-08-24 15:24:01 +0900

    pythonanywhere 배포 준비

--- start/start/settings.py
+++ start/start/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zvv%(jy8ouhpzs+d&hvt99o$x2ylqi#wz*u3#7r$cs0+@ct4ya'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
